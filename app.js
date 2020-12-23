dcf67c557411408c515042d337e5e13511ad4a5e
korede-dotcom <54069897+korede-dotcom@users.noreply.github.com>
2020-12-23 20:51:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const details = document.querySelector(".burger");
+const links = document.querySelector(".links");
+details.addEventListener("click",(e)=>{
+  links.classList.toggle("links");
+    
+})
